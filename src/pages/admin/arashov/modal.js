9fdd3a9613b85c60634c96bb25bee8d0214fd050
itkncoder <kncoderr@gmail.com>
2023-10-12 11:17:56 +0500

    f

--- src/pages/admin/arashov/modal.js
+++ src/pages/admin/arashov/modal.js
@@ -1,19 +1,19 @@
 const Modal = ({i}) => {
     return (
-        <dialog id={i._id} className="modal">
+        <dialog id={i?.email} className="modal">
             <div className="modal-box">
                 <div className="flex items-center gap-2">
-                    <img src={i.image} alt="photo" width={200} height={200} style={{width: "100px", height: "auto"}} />
-                    <h3 className="font-bold text-lg">{i.fullName}</h3>
+                    <img src={i?.image} alt="photo" width={200} height={200} style={{width: "100px", height: "auto"}} />
+                    <h3 className="font-bold text-lg">{i?.fullName}</h3>
                 </div>
                 <p className="py-4 text-lg leading-10 mt-3">
-                    {i.email} <br/>
-                    To'lov: {i.payment ? "Qilingan" : "Qilinmagan"} <br/>
-                    To'lov turi: {i.payment && i.paymentType} <br/>
-                    Tasdiqlanish: {i.verified ? "Tasdiqlangan" : "Tasdiqlanmagan"} <br/>
-                    Referal Code: {i.referralCode} <br/>
-                    Referred Code: {i.referred_code} <br/>
-                    Wallet: {i.wallet} <br/>
+                    {i?.email} <br/>
+                    To'lov: {i?.payment ? "Qilingan" : "Qilinmagan"} <br/>
+                    To'lov turi: {i?.payment && i?.paymentType} <br/>
+                    Tasdiqlanish: {i?.verified ? "Tasdiqlangan" : "Tasdiqlanmagan"} <br/>
+                    Referal Code: {i?.referralCode} <br/>
+                    Referred Code: {i?.referred_code} <br/>
+                    Wallet: {i?.wallet} <br/>
                 </p>
             </div>
             <form method="dialog" className="modal-backdrop">
