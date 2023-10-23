0c66e303985ff157d14eb5a4c1ba3fcbbb4f4d27
itkncoder <kncoderr@gmail.com>
2023-10-23 05:30:10 +0500

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,26 +39,31 @@ function Index() {
                 <Competition />
                 <Price />
                 <div className="pt-20">
-                    <div>
+                    <div className="relative z-10">
                         <p className="mezzardBold boxTitle competition__title">TRADINGNI MUKAMMAL O’RGANMOQCHI BO’SANGIZ</p>
                         <p className="mezzardBold boxTitle text-center">MA’LUMOTLARINGIZNI TO’LDIRING</p>
                     </div>
-                    <div className="flex justify-center mt-[50px]">
-                        <div className="bg-[#031026] border-[#3A7EFF] border-2 p-[40px] rounded-[35px]">
-                            <div className="bg-[#FFFFFF] w-full flex pl-2 justify-start items-center border-[1px] rounded-lg border-[#0152D1]">
-                                <i className="fa-solid fa-user bg-black text-white rounded-md px-[8px] py-[8px]"></i>
-                                <input autoComplete="off" className={"px-3 w-[320px] py-2 bg-[#FFFFFF] text-gray-900 mezzardBold form-input outline-0 rounded-r-xl"} type="text" name="fullName" placeholder="Ism Familiya" />
-                            </div>
-                            <div className="bg-[#FFFFFF] mt-[10px] w-full flex pl-2 justify-start items-center border-[1px] rounded-lg border-[#0152D1]">
-                                <i className="fa-solid fa-user bg-black text-white rounded-md px-[8px] py-[8px]"></i>
-                                <input autoComplete="off" className={"px-3 w-[320px] py-2 bg-[#FFFFFF] text-gray-900 mezzardBold form-input outline-0 rounded-r-xl"} type="text" name="fullName" placeholder="Ism Familiya" />
-                            </div>
-                            <div className="bg-[#FFFFFF] mt-[10px] w-full flex pl-2 justify-start items-center border-[1px] rounded-lg border-[#0152D1]">
-                                <i className="fa-solid fa-user bg-black text-white rounded-md px-[8px] py-[8px]"></i>
-                                <input autoComplete="off" className={"px-3 w-[320px] py-2 bg-[#FFFFFF] text-gray-900 mezzardBold form-input outline-0 rounded-r-xl"} type="text" name="fullName" placeholder="Ism Familiya" />
-                            </div>
-                            <div className="tasdiqlash_btn cursor-pointer hover:scale-105 transition-all">
-                                <p className="mezzardBold">TASDIQLASH</p>
+                    <div className="mt-[50px]">
+                        <div className="flex justify-center relative">
+                            <div className="bright1 absolute mr-[250px]"></div>
+                        </div>
+                        <div className="flex justify-center relative z-10">
+                            <div className="bg-[#031026] border-[#3A7EFF] border-2 p-[40px] rounded-[35px]">
+                                <div className="bg-[#FFFFFF] w-full flex pl-2 justify-start items-center border-[1px] rounded-lg border-[#0152D1]">
+                                    <i className="fa-solid fa-user bg-black text-white rounded-md px-[8px] py-[8px]"></i>
+                                    <input autoComplete="off" className={"px-3 w-[320px] py-2 bg-[#FFFFFF] text-gray-900 mezzardBold form-input outline-0 rounded-r-xl"} type="text" name="fullName" placeholder="Ism Familiya" />
+                                </div>
+                                <div className="bg-[#FFFFFF] mt-[10px] w-full flex pl-2 justify-start items-center border-[1px] rounded-lg border-[#0152D1]">
+                                    <i className="fa-solid fa-user bg-black text-white rounded-md px-[8px] py-[8px]"></i>
+                                    <input autoComplete="off" className={"px-3 w-[320px] py-2 bg-[#FFFFFF] text-gray-900 mezzardBold form-input outline-0 rounded-r-xl"} type="text" name="fullName" placeholder="Ism Familiya" />
+                                </div>
+                                <div className="bg-[#FFFFFF] mt-[10px] w-full flex pl-2 justify-start items-center border-[1px] rounded-lg border-[#0152D1]">
+                                    <i className="fa-solid fa-user bg-black text-white rounded-md px-[8px] py-[8px]"></i>
+                                    <input autoComplete="off" className={"px-3 w-[320px] py-2 bg-[#FFFFFF] text-gray-900 mezzardBold form-input outline-0 rounded-r-xl"} type="text" name="fullName" placeholder="Ism Familiya" />
+                                </div>
+                                <div className="tasdiqlash_btn cursor-pointer hover:scale-105 transition-all">
+                                    <p className="mezzardBold">TASDIQLASH</p>
+                                </div>
                             </div>
                         </div>
                     </div>
